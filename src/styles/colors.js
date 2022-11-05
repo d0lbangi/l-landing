871e7f2940a55488c9f496a9b4ede7f8fd0b40e8
jiUmin <wjdwkjh94@gmail.com>
2022-11-05 10:33:01 +0900

    초기세팅 추가

---
+++ src/styles/colors.js
@@ -0,0 +1,25 @@
+export const color1 = "#E3AE00";
+
+export const color2 = "#221A00";
+
+export const color3 = "#866600";
+
+export const color4 = "#4C3A00";
+
+export const color5 = "#BF9200";
+
+export const color6 = "#594400";
+
+export const color7 = "#2D2300";
+
+export const color8 = "#9F7A00";
+
+export const color9 = "#B18600";
+
+export const bgBlack = "black";
+
+export const textPrimary = "#FFFFFF";
+
+export const textSecondary = "rgba(255, 255, 255, 0.64)"; //a까지 넣으면 투명도
+
+export const textYellow = "#FFD121";
